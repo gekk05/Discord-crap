68d12b33dfff0933d2bbce176911c53ddc3cd609
gekk05 <60672155+gekk05@users.noreply.github.com>
2021-12-21 21:28:42 -0500

    Add files via upload

---
+++ discord-bridge.py
@@ -0,0 +1,43 @@
+import discord
+from discord_webhook import DiscordWebhook, DiscordEmbed
+
+HOOK_ENDPOINT = ""
+DISC_TOKEN = ""
+client = discord.Client()
+ANNOYING_PEOPLE = []
+
+@client.event
+async def on_ready():
+    print(f'Bridging as {client.user}')
+
+
+@client.event
+async def on_message(message):
+
+        url = "https://cdn.discordapp.com/avatars/{}/{}".format(str(message.author.id), message.author.avatar)
+        author = message.author
+        content = "{}".format(message.clean_content)
+
+        if message.author.id in ANNOYING_PEOPLE:
+            string_manipulated = ''
+            special_cases = '!@?# '
+            count = 0
+            for i in content:
+                if i in special_cases:
+                    string_manipulated+=i
+                    continue
+                if count % 2 == 0:
+                    string_manipulated+=i.lower()
+                    count +=1
+                else:
+                    string_manipulated+=i.upper()
+                    count +=1
+            content = string_manipulated
+
+        x = DiscordWebhook(url=HOOK_ENDPOINT, username=str(message.author).split("#")[0], content=content, avatar_url=url)
+        x.execute()
+
+
+
+
+client.run(DISC_TOKEN)
